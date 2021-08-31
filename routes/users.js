f962e02a77b1e8641f3b5266c3529a393390148a
Cesar Cisneros <cesarcisneros9@gmail.com>
2021-08-31 10:27:40 -0500

    added login user after register

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/users');
-
+const auth = require("../middleware/authentication");
 
 router.post('/login', usersController.loginUser);
 
@@ -11,7 +11,7 @@ router.get('/users', usersController.getUsers);
 //returns all the users info inculding their genre and instrument
 router.get('/user/:user_id', usersController.getUserById);
 
-router.post('/createUser', usersController.createUser)
+router.post('/createUser', usersController.createUser, usersController.loginUser)
 
 
 
